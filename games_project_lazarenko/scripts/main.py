5ca8409b8e67183e8ee08d76074c25f7f840d1ba
arseniy <sasha9299836@gmail.com>
2025-11-16 22:04:43 +0300

    ПР №1

---
+++ games_project_lazarenko/scripts/main.py
@@ -0,0 +1,7 @@
+from games_project_lazarenko.games.guess_number import play
+def main():
+    print("Добро пожаловать в игровое меню!")
+    print("=" * 30)
+    play()
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pepe!")
+
+
+if __name__ == "__main__":
+    main()
