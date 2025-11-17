b33930c9f7b914c9afa3b9d00da55f776c955554
arseniy <sasha9299836@gmail.com>
2025-11-17 09:30:53 +0300

    финалочка

--- VD_games/scripts/VD_prime.py
+++ VD_games/scripts/VD_prime.py
@@ -4,12 +4,16 @@ from VD_games.games.prime import generate_question
 
 
 def main():
+    """Главная функция для запуска игры 'Простое ли число?'."""
     name = welcome_user()
     print()
+    
     game_description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    
     if run_game(generate_question, game_description):
         print(f"Congratulations, {name}!")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
--- VD_games/scripts/VD_progression.py
+++ VD_games/scripts/VD_progression.py
@@ -1,15 +1,11 @@
 from VD_games.cli import welcome_user
 from VD_games.engine import run_game
 from VD_games.games.progression import generate_question
-
-
 def main():
     name = welcome_user()
     print()
-    game_description = "What number is missing in the progression?"
+    game_description = 'What number is missing in the progression?'
     if run_game(generate_question, game_description):
         print(f"Congratulations, {name}!")
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
